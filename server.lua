8c711514c9f98963d4a81795f61cdbf408768119
EagLeTaLoN713 <104523142+EagLeTaLoN713@users.noreply.github.com>
2022-08-06 14:07:50 +0100

    Updated server.lua
    changed resource names to bst from VORP

--- server.lua
+++ server.lua
@@ -7,15 +7,15 @@ VORP = exports.vorp_core:vorpAPI()
 
 
 
-RegisterNetEvent("VORP_Law:SendNotifications")
-AddEventHandler("VORP_Law:SendNotifications", function(players, coords)
+RegisterNetEvent("bst-law:SendNotifications")
+AddEventHandler("bst-law:SendNotifications", function(players, coords)
   
   for each, player in ipairs(players) do
     local user = VORP.getCharacter(player)
     if user ~= nil then
       local job = user.job
       if job == Config.LawJob then
-        TriggerClientEvent("VORP_Law:NotifyLaw", player, coords)
+        TriggerClientEvent("bst-law:NotifyLaw", player, coords)
       end
       
     else
@@ -26,15 +26,15 @@ AddEventHandler("VORP_Law:SendNotifications", function(players, coords)
 
 end)
 
-RegisterNetEvent("VORP_Law:CheckJob")
-AddEventHandler("VORP_Law:CheckJob", function()
+RegisterNetEvent("bst-law:CheckJob")
+AddEventHandler("bst-law:CheckJob", function()
   local _source = source
 
 	TriggerEvent("vorp:getCharacter", _source, function(user)
 		local job = user.job
 		print('Job: ' .. job)
 		if job ~= Config.LawJob then
-			TriggerClientEvent("VORP_Law:TriggerWanted", _source)
+			TriggerClientEvent("bst-law:TriggerWanted", _source)
       Debug("WantedTriggered")
       
 		end
